66eb2607ca899dd6ffc4516e4fd612601b166fe0
Dan Jecu <jdanalexandru@gmail.com>
2023-03-19 07:06:29 +0100

    prepared backend for deployment

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const mealsRouter = require('./api/meals');
 const reservationsRouter = require('./api/reservations');
 const reviewsRouter = require('./api/reviews');
 const buildPath = path.join(__dirname, '../frontend/dist');
-const port = process.env.PORT || 3000;
+
 const cors = require('cors');
 
 // For week4 no need to look into this!
--- backend/database.js
+++ backend/database.js
@@ -1,18 +1,9 @@
 require('dotenv').config();
 
-/* const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`; */
-
 // create connection
 const knex = require('knex')({
     client: 'pg',
-    connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    },
-    pool: { min: 0, max: 7 },
+    connection: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`,
 });
 
 /* {
