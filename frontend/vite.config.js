6a1e4f7ec303fe854abaa18c050544a9af1e4039
Dan Jecu <jdanalexandru@gmail.com>
2023-03-19 09:47:29 +0100

    modified vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        manifest: true,
-        rollupOptions: {
-            input: './src/main.jsx',
-        },
-    },
+
     server: {
         proxy: {
             '/api': 'https://meal-sharing-app-backend-production.up.railway.app/', // the address that u serve in the backend
