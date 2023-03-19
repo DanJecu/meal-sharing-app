d37c2d9fde2b3865901a07a620e7047ef4fbe559
Dan Jecu <jdanalexandru@gmail.com>
2023-03-19 07:47:41 +0100

    port fix for railway

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const port = parseInt(process.env.PORT, 10) || process.env.PGPORT;
+const port = process.env.PORT;
 
-app.listen('0.0.0.0:$PORT', () => {
+app.listen(port, () => {
     console.log(`Backend api available at ${process.env.API_PATH}`);
 });
