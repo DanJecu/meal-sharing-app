5fbda93a01a51304d4633c2d77740d7931ae7d71
Dan Jecu <jdanalexandru@gmail.com>
2023-03-19 07:16:51 +0100

    fixed port bug

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const app = require('./app');
 
 const port = parseInt(process.env.PORT, 10) || process.env.PGPORT;
 
-app.listen(port, () => {
-    console.log(`Backend api available at ${process.env.API_PATH}`);
-});
+app.listen(
+    /* port,  */ () => {
+        console.log(`Backend api available at ${process.env.API_PATH}`);
+    }
+);
