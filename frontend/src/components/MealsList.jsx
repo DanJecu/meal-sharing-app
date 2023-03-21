25a10749bb88684f3ee5ca5c22d5a6b731c60289
Dan Jecu <jdanalexandru@gmail.com>
2023-03-21 17:10:59 +0100

    added mobile support

--- frontend/src/components/MealsList.jsx
+++ frontend/src/components/MealsList.jsx
@@ -35,7 +35,7 @@ export default function MealsList() {
                 {meals.length ? (
                     meals.map(meal => <Meal meal={meal} key={meal.id} />)
                 ) : (
-                    <h2>There is no meal with this name 🥲</h2>
+                    <h2>There is no meal with this name 😥</h2>
                 )}
             </ul>
         </>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { TiThMenu } from 'react-icons/ti';
 import { Link } from 'react-router-dom';
 
 import styles from '../styles/components/Navbar.module.css';
 
 export default function Navbar() {
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header className={styles.navbar}>
             <Link to='/'>
@@ -13,15 +21,36 @@ export default function Navbar() {
                     <h1 className={styles.logoSharing}>SHARING</h1>
                 </span>
             </Link>
-            <ul className={styles.navigation}>
+            {/* burger menu */}
+            {isMobile && (
+                <button
+                    className={styles.btnMenu}
+                    onClick={toggleMenu}
+                    onTouchEnd={() =>
+                        window.scrollTo({ top: 0, behavior: 'smooth' })
+                    }
+                >
+                    <TiThMenu />
+                </button>
+            )}
+            <ul
+                className={styles.navigation}
+                style={{ display: isMobile && isOpen ? 'none' : 'flex' }}
+            >
                 <li>
-                    <Link to='/'>Home</Link>
+                    <Link to='/' onClick={toggleMenu}>
+                        Home
+                    </Link>
                 </li>
                 <li>
-                    <Link to='/meals'>Meals</Link>
+                    <Link to='/meals' onClick={toggleMenu}>
+                        Meals
+                    </Link>
                 </li>
                 <li>
-                    <Link to='/reviews'>Reviews</Link>
+                    <Link to='/reviews' onClick={toggleMenu}>
+                        Reviews
+                    </Link>
                 </li>
             </ul>
         </header>
