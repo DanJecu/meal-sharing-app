e94a8e07a62a8d0908874f06f148a0cd44a5b84b
Dan Jecu <jdanalexandru@gmail.com>
2023-03-23 16:32:19 +0100

    fixed env var

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({mode}) => {
+export default ({mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
 
     return {
