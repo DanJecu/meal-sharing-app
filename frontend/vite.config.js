8052a0db1508c4724b0354249df22b659f31baa4
Dan Jecu <jdanalexandru@gmail.com>
2023-03-23 16:02:15 +0100

    hide app url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
     plugins: [react()],
 
     server: {
         proxy: {
-            '/api': 'https://meal-sharing-app-backend-production.up.railway.app/',
+            '/api': env.VITE_APP_URL,
         },
     },
-});
+}   
+};
+
